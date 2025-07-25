671e425cb45624cf0fd884dd500682118188e7aa
Jordan Cooper <jordancooper1195@gmail.com>
2025-07-25 17:40:33 +1200

    Deal with some diagnostic warnings

--- init.lua
+++ init.lua
@@ -153,7 +153,7 @@ require("lazy").setup({
         words = { enabled = true },
       },
       keys = {
-        -- @module "snacks"
+        ---@module "snacks"
 
         -- Find Files
         { "<LEADER><LEADER>", function() Snacks.picker.smart() end, desc = "Smart find files" },
@@ -430,7 +430,9 @@ require("lazy").setup({
             lualine_c = { "", { "filename", path = 1, shorting_target = 60 } },
             lualine_x = {
               {
+                ---@diagnostic disable-next-line
                 noice.api.status.command.get,
+                ---@diagnostic disable-next-line
                 cond = noice.api.status.command.has,
                 color = { fg = "#ff9e64" },
               },
