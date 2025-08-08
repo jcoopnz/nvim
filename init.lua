34b9835a4a37836f5fde7aa6d5fcc3c4e160c101
Jordan Cooper <jordancooper1195@gmail.com>
2025-08-09 11:19:56 +1200

    Deal with unnecessary & loud diag warning

--- init.lua
+++ init.lua
@@ -220,6 +220,7 @@ require("lazy").setup({
       lazy = false,
       build = ":TSUpdate",
       config = function()
+        ---@diagnostic disable-next-line: missing-fields
         require("nvim-treesitter.configs").setup({
           auto_install = true,
           highlight = { enable = true },
