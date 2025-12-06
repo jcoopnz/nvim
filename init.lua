cc56e7a69193f6753f3c7aa3d5206601e49a5512
Jordan Cooper <jordancooper1195@gmail.com>
2025-12-06 20:02:02 +1300

    Reconfigure Noice

--- init.lua
+++ init.lua
@@ -328,6 +328,7 @@ require("lazy").setup({
       dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
       opts = {
         lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
@@ -335,9 +336,6 @@ require("lazy").setup({
           },
         },
         presets = {
-          command_palette = true,
-          long_message_to_split = true,
-          inc_rename = false,
           lsp_doc_border = true,
         },
         routes = { {
