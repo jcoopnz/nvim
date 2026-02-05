de9798fc9c87c46d303f2508754b134a02eb9d49
Jordan Cooper <jordancooper1195@gmail.com>
2026-02-05 13:51:41 +1300

    Adds nvim-lint

--- init.lua
+++ init.lua
@@ -488,6 +488,23 @@ require("lazy").setup({
     },
 
     {
+      "mfussenegger/nvim-lint",
+      event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+      config = function()
+        local lint = require("lint")
+        lint.linters_by_ft = {
+          javascript = { "eslint" },
+          typescript = { "eslint" },
+        }
+        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+          callback = function()
+            require("lint").try_lint()
+          end,
+        })
+      end,
+    },
+
+    {
       "nvim-lualine/lualine.nvim",
       event = "VeryLazy",
       opts = function()
